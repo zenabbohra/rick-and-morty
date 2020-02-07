f7ef026beb879947c23105822ec5f92c67ab2523
Zenab <zenab.bohra19@gmail.com>
2020-02-07 13:22:29 +1100

    Sources images from the rick and morty API doc

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
-const Card = ({ id, name, email }) => {
+const Card = ({ id, name, species, gender, status }) => {
+  console.log(status);
   return(
-    <div className='ba bw2 br2 ma2 dib bg-light-green b--light-green shadow-5'>
+    <div className='ba bw2 br2 ma2 dib bg-light-yellow b--light-yellow shadow-5'>
       <img
-        alt='robots'
-        src={`https://robohash.org/${id}?size=200x200`}
+        alt='rick-and-morty-character'
+        src={`https://rickandmortyapi.com/api/character/avatar/${id}.jpeg`}
+        style={{width:'250px', height:'250px' }}
       />
       <div>
         <h2>{name}</h2>
-        <h4>{email}</h4>
+        {
+          gender !== 'unknown' ?
+            <h4>{`${species} ${gender}`}</h4> :
+            <h4>{`${species} species`}</h4>
+        }
+        {
+          status !== 'unknown' ?
+            <h5><i>Status - {status}</i></h5> :
+            <h5><i>Status -  Yet to be found</i></h5>
+        }
+        <h5>#{id}</h5>
       </div>
     </div>
   );
