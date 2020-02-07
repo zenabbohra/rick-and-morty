26069d7bf0a5d09c83d7c1db764f8103269accdd
Zenab <zenab.bohra19@gmail.com>
2020-02-07 14:35:48 +1100

    Updates

---
+++ precache-manifest.834d124b407c6ab54a9c47b82d876f90.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6122f4fb308af2970a992285c10ca0",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "7aed8f95dcd0591fa967",
+    "url": "/rick-and-morty/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "86bbab89a24018fcb192",
+    "url": "/rick-and-morty/static/css/main.54ae0209.chunk.css"
+  },
+  {
+    "revision": "7aed8f95dcd0591fa967",
+    "url": "/rick-and-morty/static/js/2.4a15554b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rick-and-morty/static/js/2.4a15554b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86bbab89a24018fcb192",
+    "url": "/rick-and-morty/static/js/main.465f3058.chunk.js"
+  },
+  {
+    "revision": "41fa8650a88784e783f7",
+    "url": "/rick-and-morty/static/js/runtime-main.6a127776.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/rick-and-morty/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bc6cb1dea0f3e76877522724bc7d481c",
+    "url": "/rick-and-morty/static/media/rick-and-morty-pattern.bc6cb1de.jpg"
+  }
+]);
